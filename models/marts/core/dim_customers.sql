9b3e7d445d38805c1ff06b175e00a56a01a42331
caiqian.wu@gmail.com <caiqian.wu@gmail.com>
2022-01-03 19:20:30 +0000

    refactor to include payment information

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -6,6 +6,11 @@ orders as (
     select * from {{ ref('stg_orders') }}
 ),
 
+payments as (
+    select * from {{ ref('stg_payments') }}
+)
+,
+
 customer_orders as (
 
     select
@@ -13,9 +18,12 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(o.order_id) as number_of_orders,
+        sum(p.payment_amount) as lifetime_value
 
-    from orders
+    from orders o
+    left join payments p
+        on o.order_id = p.order_id
 
     group by 1
 
@@ -30,7 +38,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
 
     from customers
 
---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,10 @@
+select 
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    -- amount from Stripe is represented in cents
+    amount / 100.00 as payment_amount,
+    created as payment_created_at,
+    _batched_at as payment_batched_at
+from dbt-tutorial.stripe.payment
